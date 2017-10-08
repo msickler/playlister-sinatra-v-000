3cd0e5ef8432588d4ffb17ec67bfdfa090e12610
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 18:19:17 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ActiveRecord::Base
   extend Concerns::Slugifiable::ClassMethods
   include Concerns::Slugifiable::InstanceMethods
 
+  has_many :song_genres
   belongs_to :artist
-  has_many :genres
+  has_many :genres, through: :song_genres
 end
