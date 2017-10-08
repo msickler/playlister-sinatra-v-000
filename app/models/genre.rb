33e25a5f8544a04f4965d5ca1b07154d7e3db9ad
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 18:18:39 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,7 @@ class Genre < ActiveRecord::Base
   extend Concerns::Slugifiable::ClassMethods
   include Concerns::Slugifiable::InstanceMethods
 
-  has_many :songs
+  has_many :songs, through: :song_genres
+  has_many :song_genres
   has_many :artists, through: :songs
 end
