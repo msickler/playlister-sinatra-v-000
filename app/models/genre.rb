29a00a1c1db22df41811450800ee4dcc01dedb30
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 17:51:07 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ActiveRecord::Base
+  extend Concerns::Slugifiable::ClassMethods
+  include Concerns::Slugifiable::InstanceMethods
+
   has_many :songs
   has_many :artists, through: :songs
 end
